7a3210e4f9eb96aa296989d165ad15ab794253e7
Peter Steenbergen <psteenbergen@gmail.com>
2018-06-07 13:40:51 +0200

    Availabalityzone

---
+++ src/Model/AvailabilityZone.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TransIP\Model;
+
+/**
+ * @author Peter Steenbergen <psteenbergen@gmail.com>
+ */
+class AvailabilityZone
+{
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $country;
+
+    /**
+     * @var boolean
+     */
+    public $isDefault;
+}
